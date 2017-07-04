88c95ce12bdd93e6eff059bf528d7890081fb63f
saadbinamjad <saadbinamjad@gmail.com>
2017-07-04 06:42:18 +0600

    first commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+     println("Hello World!");
+     println("My first attempt at Kotlin. Looks good so far.");
+     
+}
